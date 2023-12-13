ee5b4b91f0780c03c2b9a75980a41d6b01d74f8a
HelmiITK <helmi220602@gmail.com>
2023-12-14 01:21:52 +0800

    get me redux, dan navbar kondisi jika ada user dan tidak ada user

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import "react-toastify/dist/ReactToastify.css";
 // import { toast } from "react-toastify";
 // import toast from "react-hot-toast";
-import { setToken } from "../reducers/authReducers";
+import { setToken, setUser } from "../reducers/authReducers";
 
 const api_url = import.meta.env.VITE_REACT_API_ADDRESS;
 
@@ -36,6 +36,46 @@ export const login = (email, password, navigate) => async (dispatch) => {
   } catch (error) {
     alert(error.message);
   }
+};
+
+export const getMe = (navigate, navigatePathSuccess, navigatePathError) => async (dispatch, getState) => {
+  try {
+    let { token } = getState().auth;
+
+    const response = await axios.get(
+      `${api_url}/profiles/my-profile`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const data = response.data;
+    // console.log(data)
+    dispatch(setUser(data));
+
+    if (navigatePathSuccess) navigate(navigatePathSuccess);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      //if token is not valid
+      if (error.response.status === 401) {
+        dispatch(logout());
+
+        if (navigatePathError) navigate(navigatePathError);
+        return;
+      }
+
+      alert(error?.response?.data?.message);
+      return;
+    }
+
+    alert(error?.message);
+  }
+}
+
+export const logout = () => (dispatch) => {
+  dispatch(setToken(null));
+  dispatch(setUser(null));
 };
 
 export const register =
