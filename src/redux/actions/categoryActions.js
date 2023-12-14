b5c0e71b4bab8bbaad6c10ce272fc2fc7a8422d4
Muhammad Azka Atqiya <90915678+azkanaon@users.noreply.github.com>
2023-12-14 20:40:38 +0700

    add enroll free course (#39)

--- src/redux/actions/categoryActions.js
+++ src/redux/actions/categoryActions.js
@@ -1,11 +1,23 @@
 import axios from "axios";
-import { setCategory } from "../reducers/categoryReducers"
+import { setCategory } from "../reducers/categoryReducers";
 
 const api_url = import.meta.env.VITE_REACT_API_ADDRESS;
 
 export const getCategory = () => async (dispatch) => {
   try {
     const response = await axios.get(`${api_url}/categories`);
+
+    const data = response.data.categories;
+
+    dispatch(setCategory(data));
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+export const getCategoryDetail = (id) => async (dispatch) => {
+  try {
+    const response = await axios.get(`${api_url}/categories/detail/${id}`);
 
     const data = response.data.categories;
 
