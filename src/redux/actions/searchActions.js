a25abdd4edb74ecb78c472d3593d0899fa5059f9
HelmiITK <89585977+HelmiITK@users.noreply.github.com>
2023-12-26 03:13:56 +0800

    Done Hit API Search, dan Slicing Halaman Search 3 mode hp, tablet dan laptop (#62)
    * first commit
    
    * change image
    
    * menyelesaikan hompage dan logika button filter
    
    * menambahkan button slide pada carousel kategori belajar
    
    * first commit
    
    * first commit
    
    * update optimasi code
    
    * add file redux
    
    * menambahkan logic pada button filter di halaman HomePage
    
    * fix bug screen pada mode mobile
    
    * ubah ukuran search bar mode mobile
    
    * migran ke redux, tetapi data api tidak lengkap overall masih 80%
    
    * menyelesaikan bug filter button
    
    * menambahkan animasi loading ketika menunggu data redux selesai didapatkan
    
    * getDetailCouse redux
    
    * menyelesaikan halaman detail mode mobile, laptop menyusul, dan hit API detailCourse
    
    * first commit and finish responsive all mode
    
    * modify style
    
    * fix buf mode mobile
    
    * slicing UI dan merapikan pada halaman CourseDetail
    
    * update consume target_audience dan prerequisite
    
    * change style navbar menu
    
    * add menu home pada navbar mobile dan tablet
    
    * update search
    
    * Menyelesaikan Hit API searching dan Slicing UI/UX halaman search 3 mode responsive hp, tablet dan laptop
    
    * finish update for staging

---
+++ src/redux/actions/searchActions.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { setSearching } from "../reducers/searchReducers";
+
+const api_url = import.meta.env.VITE_REACT_API_ADDRESS;
+
+export const getSearchCourses =
+  (errors, setErrors, search, page = 1) =>
+    async (dispatch) => {
+      try {
+        const response = await axios.get(`${api_url}/courses?page=${page}&search=${search}`);
+        const { courses, success, message } = response.data;
+        console.log(courses)
+
+        if (!success && message === "No courses found with the specified criteria") {
+          dispatch(setSearching([])); // Set array kosong untuk state searching
+          setErrors({
+            ...errors,
+            isError: true,
+            message: message,
+          });
+        } else {
+          dispatch(setSearching(courses));
+          setErrors({ ...errors, isError: false });
+        }
+
+        // dispatch(setSearching(courses));
+        // setErrors({ ...errors, isError: false });
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          setErrors({
+            ...errors,
+            isError: true,
+            message: error?.response?.data?.message || error?.message,
+          });
+        }
+
+        alert(error?.message);
+        setErrors({
+          ...errors,
+          isError: true,
+          message: error?.message,
+        });
+      }
+    };
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import categoryReducers from "./categoryReducers";
 import courseReducers from "./courseReducers";
 import authReducers from "./authReducers";
+import searchReducers from "./searchReducers";
 
 export default combineReducers({
   category: categoryReducers,
   course: courseReducers,
-  auth: authReducers
+  auth: authReducers,
+  search: searchReducers,
 });
