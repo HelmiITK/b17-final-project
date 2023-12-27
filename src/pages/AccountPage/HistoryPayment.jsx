ce37f2ff25937bf1e6f047191d1552e6e18faa52
HelmiITK <89585977+HelmiITK@users.noreply.github.com>
2023-12-27 19:19:43 +0800

    Done slicing card riwayat pembayaran (#63)
    * first commit
    
    * change image
    
    * menyelesaikan hompage dan logika button filter
    
    * menambahkan button slide pada carousel kategori belajar
    
    * first commit
    
    * first commit
    
    * update optimasi code
    
    * add file redux
    
    * menambahkan logic pada button filter di halaman HomePage
    
    * fix bug screen pada mode mobile
    
    * ubah ukuran search bar mode mobile
    
    * migran ke redux, tetapi data api tidak lengkap overall masih 80%
    
    * menyelesaikan bug filter button
    
    * menambahkan animasi loading ketika menunggu data redux selesai didapatkan
    
    * getDetailCouse redux
    
    * menyelesaikan halaman detail mode mobile, laptop menyusul, dan hit API detailCourse
    
    * first commit and finish responsive all mode
    
    * modify style
    
    * fix buf mode mobile
    
    * slicing UI dan merapikan pada halaman CourseDetail
    
    * update consume target_audience dan prerequisite
    
    * change style navbar menu
    
    * add menu home pada navbar mobile dan tablet
    
    * update search
    
    * Menyelesaikan Hit API searching dan Slicing UI/UX halaman search 3 mode responsive hp, tablet dan laptop
    
    * finish update for staging
    
    * update
    
    * done slicing card riwayat pembayaran

--- src/pages/AccountPage/HistoryPayment.jsx
+++ src/pages/AccountPage/HistoryPayment.jsx
@@ -1,9 +1,10 @@
 import 'react-toastify/dist/ReactToastify.css';
 import { useState } from "react";
+import { Book, Clock, Gem, Shield } from "lucide-react";
+import { FaStar } from "react-icons/fa";
 import HamburgerMenuAccount from "../../components/AccountComponent/HamburgerMenuAccount";
 import ButtonBack from '../../components/AccountComponent/ButtonBack';
 import MenuList from '../../components/AccountComponent/MenuList'
-import Card from '../../components/MyCourseComponent/Card';
 import Navbar from '../../components/NavbarComponent/Navbar';
 import Footer from '../../components/FooterComponent/Footer';
 
@@ -48,8 +49,45 @@ const UserPage = () => {
                            Riwayat Pembayaran
                         </div>
                         {/* card history */}
-                        <div className='flex flex-col items-center gap-4 shadow-xl rounded-3xl'>
-                           <Card />
+                        <div className='flex flex-col items-center gap-4'>
+                           <div className='flex flex-col mx-4 gap-4 pb-4 shadow-xl rounded-3xl lg:w-96 lg:h-auto'>
+                              <div>
+                                 <img
+                                    src="https://images.unsplash.com/photo-1522542550221-31fd19575a2d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                    alt=""
+                                    className='rounded-t-3xl' />
+                              </div>
+                              <div className='mx-4 flex justify-between'>
+                                 <div>
+                                    <h1 className='font-semibold text-indigo-600'>UI/UX Desegin</h1>
+                                    <h2 className='font-semibold'>Belajar Web Designer dengan Figma</h2>
+                                 </div>
+                                 <div className='flex items-start gap-1'>
+                                    <FaStar color="#F9CC00" className="w-4 h-4 lg:w-5 lg:h-5" />
+                                    <p className='font-medium'>4.7</p>
+                                 </div>
+                              </div>
+                              <div className='mx-4 flex justify-between'>
+                                 <div className='flex gap-1 items-center'>
+                                    <Shield className='text-green-400' />
+                                    <p className='text-sm font-medium'>Pemula</p>
+                                 </div>
+                                 <div className='flex gap-1 items-center'>
+                                    <Book className='text-green-400' />
+                                    <p className='text-sm font-medium'>10 Modul</p>
+                                 </div>
+                                 <div className='flex gap-1 items-center'>
+                                    <Clock className='text-green-400' />
+                                    <p className='text-sm font-medium'>120 Menit</p>
+                                 </div>
+                              </div>
+                              <div className='mx-4'>
+                                 <div className='border-none flex items-center px-4 py-1 bg-green-400 rounded-2xl justify-center gap-2 text-white font-medium text-sm'>
+                                    <Gem />
+                                    <p>Sudah Dibayar</p>
+                                 </div>
+                              </div>
+                           </div>
                         </div>
                      </div>
                   </div>
